3d2af41bb9bca998e93247e8f79abcd68b324ebf
candido <https://github.com/Doug2704/customperfume.git>
2025-02-23 17:40:16 -0300

    feat: user's service created

--- userservice/src/main/java/com/candido/userservice/controller/UserController.java
+++ userservice/src/main/java/com/candido/userservice/controller/UserController.java
@@ -1,4 +1,48 @@
 package com.candido.userservice.controller;
 
+import com.candido.userservice.entity.User;
+import com.candido.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author Douglas Candido
+ * Classe que fornece endpoints para consultas no banco de dados.
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
 public class UserController {
+    @Autowired
+    private final UserService userService;
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        try {
+            User createdUSer = userService.createUser(user);
+            return new ResponseEntity<>(createdUSer, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            throw e;
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        try {
+            User retrievedUser = userService.findById(id).get();
+            return new ResponseEntity<>(retrievedUser, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            throw e;
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
 }
--- userservice/src/main/java/com/candido/userservice/service/UserService.java
+++ userservice/src/main/java/com/candido/userservice/service/UserService.java
@@ -1,4 +1,56 @@
 package com.candido.userservice.service;
 
+import com.candido.userservice.entity.User;
+import com.candido.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Douglas Candido
+ * Classe de serviço para o banco de dados de usuário
+ * Contém apenas operações de criar, atualizar e consultar usuários.
+ * Como segurança, para deletar um usuário é necessário realizar manualmente no banco de dados.
+ */
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findById(Long id) {
+        Optional<User> retrievedUser = userRepository.findById(id);
+        try {
+            return userRepository.findById(id);
+        } catch (RuntimeException e) {
+            if (retrievedUser.isEmpty()) {
+                throw new RuntimeException("Usuário inexistente");
+            }
+            throw e;
+        }
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(Long id, User user) {
+        Optional<User> retrievedUser = userRepository.findById(id);
+        try {
+            User newUser = retrievedUser.get();
+            newUser.setNome(user.getNome());
+            newUser.setEmail(user.getEmail());
+            return userRepository.save(newUser);
+        } catch (RuntimeException e) {
+            if (retrievedUser.isEmpty()) {
+                throw new RuntimeException("Usuário inexistente");
+            }
+            throw e;
+        }
+    }
 }
